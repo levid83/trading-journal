f086f1a1bf9e9e2367cc531b49c7c79eb383398a
levi <danellevente@yahoo.com>
2017-12-05 11:37:10 +0200

    vuejs-laravel-admin integration config

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,30 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
+//needed for VueJS Laravel Admin Template
+mix.webpackConfig({
+  resolve: {
+    alias: {
+      'masonry': 'masonry-layout',
+      'isotope': 'isotope-layout'
+    }
+  },
+  // https://github.com/JeffreyWay/laravel-mix/issues/936#issuecomment-331418769
+
+ output: {
+	 chunkFilename: mix.inProduction() ? 'vendor/vuejs-laravel-admin/js/[name].[chunkhash].js' : 'vendor/vuejs-laravel-admin/js/[name].js',
+	 publicPath: "/"
+  }
+});
+mix.js('resources/vendor/vuejs-laravel-admin/main.js', 'public/vendor/vuejs-laravel-admin/js/main.js');
+/*
+mix.js('resources/assets/vuejs-admin/main.js', 'public/vuejs-admin/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
     .copy('node_modules/font-awesome/fonts/', 'public/fonts')
     .sass('node_modules/font-awesome/scss/font-awesome.scss', 'public/css')
-    .version();
+    .version();*/
+
+
+//needed for VueJS Laravel Admin Template
+// set path for production link
+mix.setResourceRoot('//localhost:8000/');
