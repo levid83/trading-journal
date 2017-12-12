5b08a9e11021351c3b4004e80e66cb5915e8e555
levi <danellevente@yahoo.com>
2017-12-12 12:47:45 +0200

    remove vuejs-laravel-admin

--- resources/assets/vuejs-laravel-admin/components/layouts/left-side/compact-menu/menu/index.js
+++
@@ -1,3 +0,0 @@
-exports.vmenu = require('./vueMenu')
-exports.vsubMenu = require('./subMenu')
-exports.vmenuItem = require('./MenuItem')
--- resources/assets/vuejs-laravel-admin/components/layouts/left-side/compact-menu/menu/vueMenu.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="vuemenu">
-        <slot></slot>
-    </div>
-</template>
